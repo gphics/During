2f6cc30449cd7b6904ef794ac041ad56c99da47d
gphics <97003589+gphics@users.noreply.github.com>
2022-08-15 11:58:35 -0700

    Fifth

--- Backend/settings.py
+++ Backend/settings.py
@@ -51,8 +51,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -139,6 +138,8 @@ USE_TZ = True
 
 # STATIC_URL = 'static/'
 # STATICFILES_DIR = os.path.join(BASE_DIR, 'static')
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 STATICFILES_ROOT = (BASE_DIR, 'staticfiles')
 STATIC_URL = 'static/'
 STATICFILES_DIRS = (BASE_DIR, 'static')
