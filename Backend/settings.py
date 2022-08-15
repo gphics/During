5cb4f23255954454b56333562ca35a18ae9e9bb4
gphics <97003589+gphics@users.noreply.github.com>
2022-08-15 12:08:45 -0700

    Seventh

--- Backend/settings.py
+++ Backend/settings.py
@@ -140,7 +140,7 @@ USE_TZ = True
 # STATICFILES_DIR = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
-STATICFILES_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
