2a968d42e93d484934d3bfe6ad9bb8e8d5526217
gphics <97003589+gphics@users.noreply.github.com>
2022-08-15 14:56:33 -0700

    now

--- Backend/settings.py
+++ Backend/settings.py
@@ -138,13 +138,20 @@ USE_TZ = True
 
 # STATIC_URL = 'static/'
 # STATICFILES_DIR = os.path.join(BASE_DIR, 'static')
-DISABLE_COLLECTSTATIC=1
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# DISABLE_COLLECTSTATIC=1
+# 
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_URL = '/static/'
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static')
+# ]
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
+STATIC_FILES_DIR = (
     os.path.join(BASE_DIR, 'static')
-]
+)
+STATIC_ROOT = os.path.join(BASE_DIR, 'live-static', 'static-root')
+MEDIA_URL = '/media/'
+MEDIA_ROOT =  os.path.join(BASE_DIR, 'live-static', 'media-root')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
